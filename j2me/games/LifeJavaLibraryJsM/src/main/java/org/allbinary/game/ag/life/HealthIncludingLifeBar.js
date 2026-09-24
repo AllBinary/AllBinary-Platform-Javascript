e834433285440c9611740833256cd7a840a39cb9
Travis Berthelot <travisberthelot@allbinary.com>
2026-09-24 05:48:21 -0500

    Correct tsconfig

---
+++ j2me/games/LifeJavaLibraryJsM/src/main/java/org/allbinary/game/ag/life/HealthIncludingLifeBar.js
@@ -0,0 +1,43 @@
+/*
+        *
+        *  AllBinary Open License Version 1
+        *  Copyright (c) 2011 AllBinary
+        *
+        *  By agreeing to this license you and any business entity you represent are
+        *  legally bound to the AllBinary Open License Version 1 legal agreement.
+        *
+        *  You may obtain the AllBinary Open License Version 1 legal agreement from
+        *  AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+        *
+        *  Created By: Travis Berthelot
+*/
+//not GWT import const HealthBarAnimation
+import { HealthBarTwodAnimation } from '../../../../../org/allbinary/game/health/HealthBarTwodAnimation.js';
+//not GWT import const HealthListenerInterface
+import { Paintable } from '../../../../../org/allbinary/graphics/paint/Paintable.js';
+//not GWT import - same folder const Life
+export class HealthIncludingLifeBar extends Paintable {
+    //@Throws(Exception.constructor)
+    static createHealthIncludingLifeBar(layerInterface, life, healthInterface, location, direction) {
+        //if statement needs to be on the same line and ternary does not work the same way.
+        return new HealthIncludingLifeBar(layerInterface, life, healthInterface, new HealthBarTwodAnimation(layerInterface, location), direction);
+    }
+    constructor(layerInterface, life, healthInterface, animationInterface, direction) {
+        super();
+        this.allbinaryLayer = layerInterface;
+        this.animationInterface = animationInterface;
+        this.life = life;
+        this.healthInterface = healthInterface;
+        this.healthInterface.addListener(this);
+        this.onHealthChange();
+    }
+    onHealthChange() {
+        this.animationInterface.onHealthChange(((this.healthInterface.getHealth() + (this.healthInterface.getMaxHealth() * this.life.get())) * this.allbinaryLayer.getWidth()) / (this.healthInterface.getMaxHealth() + (this.healthInterface.getMaxHealth() * this.life.getStartLives())));
+    }
+    paint(graphics) {
+        this.animationInterface.paintXY(graphics, 0, 0);
+    }
+    paintThreed(graphics) {
+        this.animationInterface.paintThreedXYZ(graphics, 0, 0, 0);
+    }
+}
